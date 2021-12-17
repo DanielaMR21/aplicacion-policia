79bc30ae5dc6d0bdf0f26025e0a96dc016ec79b9
Daniela Morales <ruby942010@gmail.com>
2021-12-17 08:50:30 -0500

    Creando estructura del proyecto

---
+++ aplicacion-policia/src/main/java/com/aplicacionpolicia/AplicacionPoliciaApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacionpolicia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionPoliciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicacionPoliciaApplication.class, args);
+    }
+
+}
---
+++ aplicacion-policia/src/test/java/com/aplicacionpolicia/AplicacionPoliciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacionpolicia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionPoliciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
